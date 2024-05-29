5ea0642dd52f8f21754095b794c686152f6a8b9f
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-29 11:13:41 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,8 @@
 ##删除上一个会话
 screen -X -S solxen quit
 
+SOLXEN_DIR="$HOME/solxen"
+SOLXEN_FILE="$SOLXEN_DIR/solxen-tx.yaml"
 # 更新 solxen-tx.yaml 文件
 sed -i "s|Mnemonic:.*|Mnemonic: \"$mnemonic\"|" $SOLXEN_FILE
 sed -i "s|Num:.*|Num: $num|" $SOLXEN_FILE
@@ -15,3 +17,6 @@ rm -rf $TMP_DIR
 # 启动 screen 会话并运行命令
 cd solxen
 screen -dmS solxen bash -c './solxen-tx miner'
+
+cd ~
+rm -f restart.sh
